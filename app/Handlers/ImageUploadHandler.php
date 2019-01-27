e36cfcbe7056e4054551d7ef188aea8c4651fd76
vagrant <vagrant@homestead>
2019-01-27 06:42:30 +0000

    add dingo

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -10,11 +10,49 @@ namespace App\Handlers;
 
 
 use Image;
+use Qiniu\Auth;
+use Qiniu\Storage\UploadManager;
 
 class ImageUploadHandler
 {
     // 只允许以下格式文件上传
     protected $allowed_ext = ['jpg', 'png', 'jif', 'jpeg'];
+
+    public function qiniuSave($file, $folder, $file_prefix, $max_width=false)
+    {
+        // 七牛鉴权类
+        // 用于签名的公钥和私钥
+        $accessKey = config('qiniu.Access_Key');
+        $secretKey = config('qiniu.Secret_Key');
+        // 初始化签权对象
+        $auth = new Auth($accessKey, $secretKey);
+        $bucket = config('qiniu.Bucket_Name');
+        $domain = config('qiniu.Domain');
+        // 生成上传Token
+        $token = $auth->uploadToken($bucket);
+        // 构建 UploadManager 对象
+        $uploadMgr = new UploadManager();
+        // 构建存储的文件夹规则，值如：images/avatars/201709/21/
+        // 文件夹切割能让查找效率更高。
+        $folder_name = "images/$folder/" . date("Ym", time()) . '/' . date("d", time()) . '/';
+        // 文件的上传路径
+        $upload_path = $file->getPathname();
+        // 获取文件的后缀名，因图片从剪切板里粘贴时后缀名为空，所以此处确保后缀一直存在
+        $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
+        // 拼接文件名，加前缀是为了增加辨析度，前缀可以是相关数据模型的 ID
+        $filename = $folder_name . $file_prefix . '_' . time() . '_' .str_random(10) . '.' . $extension;
+        // 如果上传的不是图片将终止操作
+        if (!in_array($extension, $this->allowed_ext)){
+            return false;
+        }
+        $uploadMgr->putFile($token, $filename, $upload_path);
+        $url = $domain . $filename;
+        // 如果限制了图片宽度，就进行裁剪
+        if ($max_width && $extension != 'gif') {
+            $url = $domain . $filename . '/1/w/' . $max_width;
+        }
+        return $url;
+    }
 
     public function save($file, $folder, $file_prefix, $max_width=false)
     {
