74c8a729131c1c22aed2ec432841dce1f48efd63
vagrant <vagrant@homestead>
2019-02-21 14:36:09 +0000

    add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -12,7 +12,19 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->input('phone');
+        $captchaData = Cache::get($request->captcha_key);
+
+        if (!$captchaData) {
+            return $this->response->error('图片验证码已失效', 422);
+        }
+
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+
+        $phone = $captchaData['phone'];
 
         // 生成4位随机数，左侧补0
         $code = str_pad(random_int(1,9999), 4, 0, STR_PAD_LEFT);
@@ -30,10 +42,12 @@ class VerificationCodesController extends Controller
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10 分钟过期
         Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+        // 清除图片验证码缓存
+        Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
-            'expired_at' => $expiredAt,
+            'expired_at' => $expiredAt->toDateTimeString(),
         ])->setStatusCode(201);
     }
 }
